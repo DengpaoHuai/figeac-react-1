e139827c346f7c348262de0376cae9036dcc88d4
Pierrick Hauguel <pierrickhauguel@outlook.com>
2024-09-17 11:28:55 +0200

    correction

--- src/pages/CreateWine.tsx
+++ src/pages/CreateWine.tsx
@@ -1,6 +1,10 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { Wine } from "../types/wine.type";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { WineContext } from "../contexts/WineContextProvider";
 
 const wineSchema = z.object({
   name: z.string().min(3).max(10, "Trop long"),
@@ -8,23 +12,39 @@ const wineSchema = z.object({
   degree: z.coerce.number().min(0).max(25),
 });
 
-type Wine = z.infer<typeof wineSchema>;
-
 const CreateWine = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<Wine>({
+  } = useForm<Omit<Wine, "_id">>({
     resolver: zodResolver(wineSchema),
   });
+  const navigate = useNavigate();
+  const { createWine } = useContext(WineContext);
 
-  const onSubmit = (values: Wine) => {
+  const onSubmit = (values: Omit<Wine, "_id">) => {
     console.log(values);
+    fetch("https://crudcrud.com/api/7f061ddf3d1548d6aea97b41cd358664/wines", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    }).then((response) => {
+      response.json().then((data: Wine) => {
+        createWine(data);
+        navigate("/list-wine");
+      });
+    });
   };
 
   return (
-    <div>
+    <div
+      style={{
+        height: "300vh",
+      }}
+    >
       <h1>Create Wine</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <input
